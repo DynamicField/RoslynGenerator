905420ed841ef26d0a2aec96866694f6964479f6
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2019-07-07 16:11:25 +0200

    Remove files for gh-pages

--- RoslynGenerator/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿var emojis = ["👾", "🐧", "👏", "👩‍💻"];
-var randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-document.getElementById("loadingEmoji").textContent = randomEmoji;
-window.monacoEvents = {
-    onChange: function (id) {
-        var editor = window.Blazaco.Editors.find(e => e.id === id);
-        editor.editor.onDidChangeModelContent(() => DotNet.invokeMethod("RoslynGenerator", "InvokeTextChanged", id));
-    }
-}
