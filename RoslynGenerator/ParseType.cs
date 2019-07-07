a2b6b66cf1bceb21123250bb4d066c69b09e9b56
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2019-07-07 15:57:03 +0200

    Add project files.

---
+++ RoslynGenerator/ParseType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoslynGenerator
+{
+    public enum ParseType
+    {
+        Compilation,
+        Member,
+        Expression,
+        Statement
+    }
+}
---
+++ RoslynGenerator/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿var emojis = ["👾", "🐧", "👏", "👩‍💻"];
+var randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+document.getElementById("loadingEmoji").textContent = randomEmoji;
+window.monacoEvents = {
+    onChange: function (id) {
+        var editor = window.Blazaco.Editors.find(e => e.id === id);
+        editor.editor.onDidChangeModelContent(() => DotNet.invokeMethod("RoslynGenerator", "InvokeTextChanged", id));
+    }
+}
